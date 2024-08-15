4f0cfa4c8eeff47f6ee52bf13086671ed04acf05
Vahit Bugra YESILKAYNAK <bugrayesilkaynak@gmail.com>
2024-08-15 07:41:11 +0200

    implement full experiment run logic excluding some metrics

--- shadow_segmentation_experiment.py
+++ shadow_segmentation_experiment.py
@@ -1,3 +1,7 @@
+import os
+from typing import List, Optional
+
+import matplotlib.pyplot as plt
 import numpy as np
 
 from random_forest_segmentor import Segmentor
@@ -11,10 +15,56 @@ from utils import (
 )
 
 SEED = 42
-NUM_PATCHES_EACH_SPLIT = 10
+NUM_PATCHES_EACH_SPLIT = 10000
 PATCH_SIZE = 5
 
+NUM_SAMPLE_IMAGES_TO_SAVE = 5
+OUTPUT_PATH = "shadow_segmentation_experiment_results"
+os.makedirs(OUTPUT_PATH, exist_ok=True)
+
 np.random.seed(SEED)
+
+
+def run_experiment(
+    train_image_splits: List[np.ndarray],
+    train_shadow_map_splits: List[np.ndarray],
+    train_conf_map_splits: Optional[List[np.ndarray]],
+    patch_idxs: List[List[tuple]],
+    test_images: np.ndarray,
+):
+
+    train_patches, train_labels = generate_patch_dataset(
+        {
+            "image": train_image_splits,
+            "shadow_map": train_shadow_map_splits,
+            "conf_map": train_conf_map_splits,
+        },
+        PATCH_SIZE,
+        patch_idxs,
+    )
+
+    segmentor = Segmentor(seed=SEED)
+    segmentor.train(train_patches, train_labels)
+
+    predictions = np.zeros(
+        (
+            test_images.shape[0],
+            test_images.shape[1] - PATCH_SIZE + 1,
+            test_images.shape[2] - PATCH_SIZE + 1,
+        ),
+        dtype=np.uint8,
+    )
+    for i, test_image in enumerate(test_images):
+        test_image_patches = patchify_single_image(test_image, PATCH_SIZE)
+        test_image_predictions = segmentor.predict(test_image_patches)
+        test_shadow_map_predictions = depatchify_single_image_prediction(
+            test_image_predictions, test_image.shape, PATCH_SIZE
+        )
+
+        predictions[i] = test_shadow_map_predictions
+
+    return predictions
+
 
 if __name__ == "__main__":
 
@@ -83,8 +133,8 @@ if __name__ == "__main__":
     liver_splits = split_views(liver_images, liver_split_idxs)
 
     # Split shadow maps
-    shadow_map_spine = split_views(shadow_map_spine, spine_split_idxs)
-    shadow_map_liver = split_views(shadow_map_liver, liver_split_idxs)
+    shadow_map_spine_splits = split_views(shadow_map_spine, spine_split_idxs)
+    shadow_map_liver_splits = split_views(shadow_map_liver, liver_split_idxs)
 
     # Generate random idxs for patches
     spine_patch_idxs = []
@@ -107,26 +157,46 @@ if __name__ == "__main__":
             y = np.random.randint(0, split.shape[2] - PATCH_SIZE)
             liver_patch_idxs[-1].append((d, x, y))
 
-    # Conduct experiment without confidence maps
-    print("Experiment without confidence maps")
-    print("Spine:")
+    # Run experiments
 
-    patches, labels = generate_patch_dataset(
-        {"image": spine_splits, "shadow_map": shadow_map_spine},
-        PATCH_SIZE,
-        spine_patch_idxs,
-    )
-    segmentor = Segmentor(seed=SEED)
-    segmentor.train(patches, labels)
+    print("No Confidence Map - Spine")
+    for idx, split in enumerate(spine_splits):
+        test_images = split
+        test_shadow_map = shadow_map_spine_splits[idx]
 
-    print(f"Patches shape: {patches.shape}, Labels shape: {labels.shape}")
+        train_image_splits = spine_splits[:idx] + spine_splits[idx + 1 :]
+        train_shadow_map_splits = (
+            shadow_map_spine_splits[:idx] + shadow_map_spine_splits[idx + 1 :]
+        )
+        train_conf_map_splits = None
+        train_patch_idxs = spine_patch_idxs[:idx] + spine_patch_idxs[idx + 1 :]
 
-    print("Liver:")
+        predictions = run_experiment(
+            train_image_splits,
+            train_shadow_map_splits,
+            train_conf_map_splits,
+            train_patch_idxs,
+            test_images,
+        )
 
-    patches, labels = generate_patch_dataset(
-        {"image": liver_splits, "shadow_map": shadow_map_liver},
-        PATCH_SIZE,
-        liver_patch_idxs,
-    )
+        trimmed_ground_truth = test_shadow_map[
+            :, PATCH_SIZE // 2 : -(PATCH_SIZE // 2), PATCH_SIZE // 2 : -(PATCH_SIZE // 2)
+        ]
 
-    print(f"Patches shape: {patches.shape}, Labels shape: {labels.shape}")
+        print(f"Spine - No Confidence Map - Split {idx}")
+        print(f"Accuracy: {np.mean(predictions == trimmed_ground_truth)}")
+        print(
+            f"Pred Shape: {predictions.shape}, GT Shape: {trimmed_ground_truth.shape}"
+        )
+
+        os.makedirs(os.path.join(OUTPUT_PATH, "spine_no_cm"), exist_ok=True)
+        for i, prediction in enumerate(predictions[:NUM_SAMPLE_IMAGES_TO_SAVE]):
+            plt.imsave(
+                os.path.join(
+                    OUTPUT_PATH,
+                    "spine_no_cm",
+                    f"split_{idx}_prediction_{i}.png",
+                ),
+                prediction,
+                cmap="gray",
+            )
